98f0544c27850768c9702e3a23754dccc9a38e21
Oliver Park <96828703+oliver0828-dev@users.noreply.github.com>
2024-06-08 14:23:49 +0900

    Add files via upload

---
+++ iTour/iTour/Sight.swift
@@ -0,0 +1,18 @@
+//
+//  Sight.swift
+//  iTour
+//
+//  Created by Oliver Park on 6/8/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Sight {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
